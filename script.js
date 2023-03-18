0b4e6f3be4905315d85440d6bb0646e7a05b0556
safatsaat2 <sakibsafat47@gmail.com>
2023-03-18 15:57:20 +0600

    img changing

---
+++ script.js
@@ -0,0 +1,17 @@
+const imgContainer = document.getElementById('img-container');
+const product1 = () =>
+    imgContainer.innerHTML = `<img class="w-full rounded-md cursor-pointer" src="images/image-product-1.jpg" alt="">`;
+
+
+const product2 = () =>
+    imgContainer.innerHTML = `<img class="w-full rounded-md cursor-pointer" src="images/image-product-2.jpg" alt="">`;
+
+
+const product3 = () =>
+    imgContainer.innerHTML = `<img class="w-full rounded-md cursor-pointer" src="images/image-product-3.jpg" alt="">`;
+
+
+const product4 = () =>
+    imgContainer.innerHTML = `<img class="w-full rounded-md cursor-pointer" src="images/image-product-4.jpg" alt="">`;
+
+    const body = document.getElementById();
