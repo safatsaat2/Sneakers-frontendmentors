f28b0bd587b06808e4d2ffd38f7c9335456edcae
safatsaat2 <sakibsafat47@gmail.com>
2023-03-18 16:42:24 +0600

    cart quantity done

--- script.js
+++ script.js
@@ -1,18 +1,18 @@
 const imgContainer = document.getElementById('img-container');
 const product1 = () =>
-    imgContainer.innerHTML = `<img class="w-full rounded-md cursor-pointer" src="images/image-product-1.jpg" alt="">`;
+    imgContainer.innerHTML = `<label for="my-modal-3"><img class="w-full rounded-md cursor-pointer" src="images/image-product-1.jpg" alt=""></label>`;
 
 
 const product2 = () =>
-    imgContainer.innerHTML = `<img class="w-full rounded-md cursor-pointer" src="images/image-product-2.jpg" alt="">`;
+    imgContainer.innerHTML = `<label for="my-modal-3"><img class="w-full rounded-md cursor-pointer" src="images/image-product-2.jpg" alt=""></label>`;
 
 
 const product3 = () =>
-    imgContainer.innerHTML = `<img class="w-full rounded-md cursor-pointer" src="images/image-product-3.jpg" alt="">`;
+    imgContainer.innerHTML = `<label for="my-modal-3"><img class="w-full rounded-md cursor-pointer" src="images/image-product-3.jpg" alt=""></label>`;
 
 
 const product4 = () =>
-    imgContainer.innerHTML = `<img class="w-full rounded-md cursor-pointer" src="images/image-product-4.jpg" alt="">`;
+    imgContainer.innerHTML = `<label for="my-modal-3"><img class="w-full rounded-md cursor-pointer" src="images/image-product-4.jpg" alt=""></label>`;
 
     let amountNum = parseInt(document.getElementById('amount-num').innerText);
 const plus = () => {
@@ -24,5 +24,11 @@ const minus = () => {
         document.getElementById('amount-num').innerText = (amountNum--) - 1;
     }
 };
+let quantity = parseInt(document.getElementById('quantity').innerText);
+const addToCart = () => {
+    if(amountNum !== 0){
+        document.getElementById('quantity').innerText = (quantity++)+ 1;
+    }
+}
     
     
