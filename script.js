cb9e1b70a533163c452e0f5848a32b13e3787cc0
safatsaat2 <sakibsafat47@gmail.com>
2023-03-18 17:49:06 +0600

    done

--- script.js
+++ script.js
@@ -45,10 +45,15 @@ const addToCart = () => {
                 <p class="text-orange-500 pl-4">$125.00 ✕ ${amountNum}</p>
             <p class="font-bold">$${125 * amountNum}</p>
             </div>
-            <button onclick="shesh()" class='px-4 py-2 cursor-pointer rounded-lg text-white bg-orange-500 mt-4 ml-6'>Checkout</button>
+            <button onclick="shesh()" class='px-4 py-2 cursor-pointer rounded-lg text-white bg-orange-500 mt-4 ml-6'>clear</button>
+
         </div>
     </div>`
     }
+}
+
+function shesh(){
+    location.reload()
 }
 
 
